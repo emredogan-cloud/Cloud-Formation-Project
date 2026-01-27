6a76c5d68e1043745b1360bbdf7d4b642c4dec03
emredogan-cloud <ed897854@gmail.com>
2026-01-27 22:16:49 +0300

    utils/session.py and utils/logging.py were included in the project, reducing the responsibility for the app.py file.

---
+++ utils/session.py
@@ -0,0 +1,36 @@
+import boto3
+from typing import TYPE_CHECKING, overload, Literal
+
+if TYPE_CHECKING:
+    from mypy_boto3_ec2 import EC2Client
+    from mypy_boto3_sns import SNSClient
+
+AWSService = Literal['ec2','sns']
+
+class AWSSessionManager:
+    _instance = None
+    
+    def __init__(self):
+        self._session = {}
+
+    @classmethod
+    def get_instance(cls):
+        if cls._instance is None:
+            cls._instance = cls()
+        return cls._instance
+
+    def get_session(self, region: str = "us-east-1") -> boto3.Session:
+        if region not in self._session:
+            self._session[region] = boto3.Session(region_name=region)
+        return self._session[region]
+
+    
+    @overload
+    def get_client(self, service_name: Literal['ec2'], region: str = "us-east-1") -> "EC2Client": ...
+
+    @overload
+    def get_client(self, service_name: Literal['sns'], region: str = "us-east-1") -> "SNSClient": ...
+
+    def get_client(self, service_name: AWSService, region: str = "us-east-1"):
+        session = self.get_session(region)
+        return session.client(service_name)
